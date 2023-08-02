f061ded983f0b4a3c1bd2d40a0858feff71e6143
Surya-V-28 <suryaarmy231@gmail.com>
2023-08-02 18:01:42 +0530

    exceptions and logging

---
+++ src/exception.py
@@ -0,0 +1,31 @@
+
+
+import sys
+import logging 
+def error_message_detail(error,error_detial:sys):
+
+    _,_,exec_tb = error_detial.exc_info()
+    file_name = exec_tb.tb_frame.f_code.co_filename
+    error_message = "Error Occured in the Python Scripts Name [{0}] LineNumber [{1}] error [{2}]".format(
+        file_name,exec_tb.tb_lineno,str(error)
+    )
+    return error_message
+
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message  = error_message_detail(error_message,error_detial=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+# if __name__ =='__main__':
+    # try:
+        # a=1/0
+    # except Exception as e:
+        # logging.info("Zero Divide Exceptions")
+        # raise CustomException(e,sys)
+# 
